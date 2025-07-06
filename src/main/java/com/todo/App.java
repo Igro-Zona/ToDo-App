a450db9c7f42f2bbbfc0f101c5d20b82124d1a51
Igro-Zona <tim.bic22@gmail.com>
2025-07-06 04:15:07 +0300

    build: created App.java file to start the program, also fixed modules issues

--- src/main/java/com/todo/App.java
+++ src/main/java/com/todo/App.java
@@ -8,9 +8,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
     private static Scene scene;
@@ -29,10 +26,6 @@ public class App extends Application {
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.todo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
     opens com.todo to javafx.fxml;
+
     exports com.todo;
 }
