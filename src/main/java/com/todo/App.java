dd35376edce0945dcafcd6b410528d677f294839
Igro-Zona <tim.bic22@gmail.com>
2025-07-09 04:44:36 +0300

    build: updated stage settings for correct display

--- src/main/java/com/todo/App.java
+++ src/main/java/com/todo/App.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -13,10 +14,18 @@ public class App extends Application {
     private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("main"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        try {
+            scene = new Scene(loadFXML("main"), 1120, 630);
+            stage.setScene(scene);
+            stage.setTitle("Todo-App");
+            stage.initStyle(StageStyle.DECORATED);
+            stage.setMinWidth(1120);
+            stage.setMinHeight(630);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     static void setRoot(String fxml) throws IOException {
